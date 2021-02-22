24e9cbcb08508b3b633df4b1a9e7d1e57ca0424e
Luis Menendez <lmenendez.1@gmail.com>
2021-02-22 11:27:23 -0600

    Update views.py

--- holamundo/views.py
+++ holamundo/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 #utilities
 from datetime import datetime
-
+import csv, io, json
 def hello_world(request):
     now = datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
     return HttpResponse('Oh, hie current server time is {now}'.format(now=now))
@@ -11,4 +11,8 @@ def hello_world(request):
 def hi(request):
     """Hi"""
     numbers = request.GET['numbers']
-    return HttpResponse(str(numbers))
+    reader = csv.DictReader(io.StringIO(numbers))
+
+    json_data = json.dumps(list(reader))
+    
+    return HttpResponse(json_data)
