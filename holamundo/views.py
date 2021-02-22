a24858e8443a3fc75eab8c5969011091e26d51a3
Luis Menendez <lmenendez.1@gmail.com>
2021-02-22 10:36:40 -0600

    Update views.py

--- holamundo/views.py
+++ holamundo/views.py
@@ -6,6 +6,6 @@ from datetime import datetime
 
 def hello_world(request):
     now = datetime.now()
-    return HttpResponse('Oh, hie current server time is {now}'.format(now=str(now=)))
+    return HttpResponse('Oh, hie current server time is {now}'.format(now=str(now)))
 
 
