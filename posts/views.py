e52e161eda9847bee04c13dee9ad8db9a9fc527e
Luis Menendez <lmenendez.1@gmail.com>
2021-02-23 10:56:52 -0600

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,35 @@
 #Django
 from django.http import HttpResponse
 
+posts = [
+    {
+        'name': 'Mont Blac',
+        'user': 'Yésica Cortés',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Vía Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    },
+]
+
 def list_posts(request):
     """Lists existing posts."""
-    posts = [1, 2 ,4]
-    return HttpResponse(str(posts))
+    content = []
+    for post in posts:
+        content.append("""
+        <p><strong>{name}</strong></p>
+        <p><small>{user} - <i>{timestamp}</i></small></p>
+        <figure><img src="{picture}"/>)</figure>
+        """.format(**post))
+    return HttpResponse('<br>'.join(content))
 # Create your views here.
