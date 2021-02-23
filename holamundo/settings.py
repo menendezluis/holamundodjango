7ee26dcc69afdeede5759cc4f81d1a918649eb52
Luis Menendez <lmenendez.1@gmail.com>
2021-02-23 10:29:40 -0600

    posts

--- holamundo/settings.py
+++ holamundo/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = ['34.72.201.231']
 # Application definition
 
 INSTALLED_APPS = [
+    #DJango apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local Apps
+    'posts',
 ]
 
 MIDDLEWARE = [
--- posts/apps.py
+++ posts/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
+    """Post application settings."""
     name = 'posts'
+    varbose_name = 'Posts'
